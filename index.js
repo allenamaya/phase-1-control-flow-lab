2d14468ff5744b1bfbf41cb8cf65db4f84ed026e
allenamaya <ahlee.amaya@gmail.com>
2023-03-24 21:28:01 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rides){
+    if (rides <= 400) {
+      return 'This one is on me!';
+    } else if (rides > 2500) {
+      return 'No can do.';
+    } else if (rides > 2000) {
+      return 'I will gladly take your thirty bucks.';
+    }
+  }
+
+function ternaryCheckCity(city){
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+switch(tip){
+  
+   case "generous":
+    return 'Thank you so much.';
+   
+   case "not as generous":
+    return 'Thank you.';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+   default:
+    return 'Bye.';
+
+}
 }
